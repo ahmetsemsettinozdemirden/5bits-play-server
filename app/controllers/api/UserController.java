835fc4a4e923bae2a08d5b4ad16b2ae7d3c4ee9d
Murat Karyağdı <murat@uncosoft.com>
2019-06-14 14:03:27 +0300

    controller moved

--- app/controllers/api/UserController.java
+++ app/controllers/api/UserController.java
@@ -1,4 +1,12 @@
 package controllers.api;
 
-public class UserController {
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class UserController extends Controller {
+
+    public Result login() {
+
+        return null;
+    }
 }
